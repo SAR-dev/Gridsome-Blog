e225c7471b60cd861438f58c22fdb81325746bda
Sayed Ar Rafi <34175729+sayed-rafi@users.noreply.github.com>
2020-03-17 23:33:07 +0600

    up

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -8,8 +8,6 @@
 
 <script>
 import VueSlickCarousel from "vue-slick-carousel";
-import "vue-slick-carousel/dist/vue-slick-carousel.css";
-import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
 
 export default {
   props: ["images"],
@@ -39,4 +37,5 @@ export default {
         background-color: #ffc20e;
         display: block;
     }
+    @import "../css/carousel.css"
 </style>
